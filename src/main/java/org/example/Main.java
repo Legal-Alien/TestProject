e42de4d61f80179dda38e0ff03abf8cb0ec28df5
Legal-Alien <j.moniz@outlook.com>
2023-10-05 23:35:11 +0200

    comment toegevoegd in Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,5 @@
 package org.example;
-
+//test comment voor het stagen, committen en pushen etc.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
